27720be8c557d44111154bfcbc5978ec4260800a
amatgil <amatgilvinyes@gmail.com>
2023-10-16 01:15:14 +0200

    Minor changes

--- tic_tac_toe.lisp
+++ tic_tac_toe.lisp
@@ -5,41 +5,41 @@
 (defparameter *turn* :cross)
 
 (defclass board ()
-  ((espais 
-    :initargs :espais
+  ((spaces 
+    :initargs :spaces
     ; Espai és :empty, :cross o :naught
     :initform (loop for i to (- (* +board-width+ +board-width+) 1) collecting :empty) ; 0 to 8 = 9 
-    :accessor espais)))
+    :accessor :spaces)))
   
-(defun estat->char (estat)
-  (ecase estat
+(defun state->char (state)
+  (ecase state
    (:empty " ")
    (:cross "X")
    (:naught "O")
-   (t (error "Estat imprevist"))))
+   (t (error "Unregistered state"))))
 
 (defun pos->idx (pos)
   (+ (first pos) (* (second pos) +board-width+)))
 
 (defmethod print-board ((b board))
-  (with-slots (espais) b
+  (with-slots (spaces) b
    (loop 
-    for item in espais
-    for i from 1 to (length espais)
+    for item in spaces
+    for i from 1 to (length spaces)
     do (progn 
-        (format t "|~a" (estat->char item))
+        (format t "|~a" (state->char item))
         (when (zerop (mod i +board-width+)) (format t "|~%"))))))
     
 (defmethod play-move ((b board) idx)
-  (with-slots (espais) b
-   (if (eql (elt espais idx) :empty)
+  (with-slots (spaces) b
+   (if (eql (elt spaces idx) :empty)
     (progn 
-     (setf (elt espais idx) *turn*)
+     (setf (elt spaces idx) *turn*)
      (switch-turn))
-    (format t "Espai ja ocupat!~%"))))
+    (format t "Space already occupied!~%"))))
 
-(defmacro comprovar-files (tiles)
-  "Genera el codi que compara els elements de cada fila entre ells"
+(defmacro check-rows (tiles)
+  "Generates the code that checks each of the row's elements amongst themselves"
   (or
    (dotimes (i +board-width+) 
     (and
@@ -52,28 +52,28 @@
 ;                   (and
 ;                    (eql (elt tiles (+ 0 (* i +board-width+))) (elt tiles (+ 1 (* i +board-width+))))
 ;                    (eql (elt tiles (+ 1 (* i +board-width+))) (elt tiles (+ 2 (* i +board-width+))))
-;                    (not (eql (elt espais (* i +board-width+)) :empty))))))
+;                    (not (eql (elt spaces (* i +board-width+)) :empty))))))
    
-(defmethod check-victory ((b board))
-  (with-slots (espais) b
-   (or
-    (comprovar-files espais))))
-     
-;(defmethod check-victory ((b board))
-;  (with-slots (espais) b
+;(defmethod check-victory-m ((b board))
+;  (with-slots (spaces) b
 ;   (or
-;    (and
-;     (eql (elt espais (+ 0 (* 0 +board-width+))) (elt espais (+ 1 (* 0 +board-width+))))
-;     (eql (elt espais (+ 1 (* 0 +board-width+))) (elt espais (+ 2 (* 0 +board-width+))))
-;     (not (eql (elt espais (* 0 +board-width+)) :empty)))
-;    (and
-;     (eql (elt espais (+ 0 (* 1 +board-width+))) (elt espais (+ 1 (* 1 +board-width+))))
-;     (eql (elt espais (+ 1 (* 1 +board-width+))) (elt espais (+ 2 (* 1 +board-width+))))
-;     (not (eql (elt espais (* 1 +board-width+)) :empty)))
-;    (and
-;     (eql (elt espais (+ 0 (* 2 +board-width+))) (elt espais (+ 1 (* 2 +board-width+))))
-;     (eql (elt espais (+ 1 (* 2 +board-width+))) (elt espais (+ 2 (* 2 +board-width+))))
-;     (not (eql (elt espais (* 2 +board-width+)) :empty))))))
+;    (check-rows spaces))))
+     
+(defmethod check-victory ((b board))
+  (with-slots (spaces) b
+   (or
+    (and
+     (eql (elt spaces (+ 0 (* 0 +board-width+))) (elt spaces (+ 1 (* 0 +board-width+))))
+     (eql (elt spaces (+ 1 (* 0 +board-width+))) (elt spaces (+ 2 (* 0 +board-width+))))
+     (not (eql (elt spaces (* 0 +board-width+)) :empty)))
+    (and
+     (eql (elt spaces (+ 0 (* 1 +board-width+))) (elt spaces (+ 1 (* 1 +board-width+))))
+     (eql (elt spaces (+ 1 (* 1 +board-width+))) (elt spaces (+ 2 (* 1 +board-width+))))
+     (not (eql (elt spaces (* 1 +board-width+)) :empty)))
+    (and
+     (eql (elt spaces (+ 0 (* 2 +board-width+))) (elt spaces (+ 1 (* 2 +board-width+))))
+     (eql (elt spaces (+ 1 (* 2 +board-width+))) (elt spaces (+ 2 (* 2 +board-width+))))
+     (not (eql (elt spaces (* 2 +board-width+)) :empty))))))
   
 (defun switch-turn ()
   (if (eql *turn* :cross)
@@ -95,7 +95,7 @@
 
 
 (print-board *game-board*)
-(format t "Li toca a: ~w~%" (estat->char *turn*))
+(format t "It is now ~w's turn~%" (state->char *turn*))
  
 
 (loop
@@ -105,16 +105,16 @@
         (progn 
          (clear-term)
          (print-board *game-board*)
-         (format t "La entrada ha de ser un número entra 1 i 9. Torna-ho a intentar~%"))
+         (format t "Input must be a number between 1 i 9 inclusive. Try again~%"))
         (progn
          (clear-term)
          (play-move *game-board* input)
          (print-board *game-board*)
-         (format t "Li toca a: ~w~%" (estat->char *turn*))))
+         (format t "It is now ~w's turn~%" (state->char *turn*))))
        (when (check-victory *game-board*) 
         (progn
-         (switch-turn) ;ens hem passat!
-         (format t "La partida ha acabat! Han guanyat: les ~as!~%" (estat->char *turn*))
+         (switch-turn) ; we changed when we shouldn't've. change back
+         (format t "Game over! The winner is: ~as!~%" (state->char *turn*))
          (return)))))
          
 
